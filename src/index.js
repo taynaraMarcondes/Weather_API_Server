25deaeb66877c00c0f923e0eda11623e2b36053e
taynaraMarcondes <taynaramarcondes@yahoo.com.br>
2021-03-02 23:58:31 -0300

    initial configurations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+//const axios = require('axios')
 
 const app = express()
 
 app.use(cors())
+
+//app.get('/', async())
 
 app.listen(8080, () => console.log('Server is running!'))
