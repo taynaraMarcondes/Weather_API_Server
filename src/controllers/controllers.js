5bcbfa5a6ea39a9f63d6a296c197250edc894415
taynaraMarcondes <taynaramarcondes@yahoo.com.br>
2021-03-03 12:42:36 -0300

    minor fixes

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,7 +1,7 @@
 const Controller = {}
 
 Controller.getCityDetails = async (req, res) => {
-    const currentWeather = await axios(`api.openweathermap.org/data/2.5/weather?q=${req}&units=metric&appid=1c931d8004094ad110ed18c2f81e3ad3`)
+    const currentWeather = await axios.get(`api.openweathermap.org/data/2.5/weather?q=${req}&units=metric&appid=${API_KEY}`)
     const cityDetails = {
         city: currentWeather.name,
         country: currentWeather.sys.country,
