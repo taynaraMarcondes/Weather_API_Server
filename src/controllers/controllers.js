001db69b53e7a17dbeba29642a2facaefc5546bc
taynaraMarcondes <taynaramarcondes@yahoo.com.br>
2021-03-03 19:35:07 -0300

    insert getCityDetails

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,21 +1,23 @@
-const Controller = {}
 const dotenv = require('dotenv')
 const axios = require('axios')
+const { errorMessages, generateError } = require('../common/errors')
 dotenv.config()
+
+const Controller = {}
 
 Controller.getCityDetails = async (req, res) => {
     try {
         const { city } = req.params
         const currentWeather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}`)
-        console.log('aqui', currentWeather)
         if (currentWeather.status === 200) {
             const data = currentWeather.data
+
             const cityDetails = {
                 city: data?.name ?? city,
                 country: data?.sys?.country ?? '-',
                 temperature: data?.main?.temp ?? '-',
                 humidity: data?.main?.humidity ?? '-',
-                currentWeather: data?.weather?.main ?? '-',
+                currentWeather: data?.weather[0]?.main ?? '-',
             }
             return res.status(200).send(cityDetails)
         } else {
@@ -24,13 +26,9 @@ Controller.getCityDetails = async (req, res) => {
 
     } catch (e) {
         console.log(e)
-        //return res.status(e.status).send(e.data.message)
-        return res.send(e)
+        return generateError(res, 400, e.message)
     }
 }
 
-Controller.get = async (req, res) => {
-    return res.send('Conseguiu!')
-}
 
 module.exports = Controller
--- src/interfaces/routes/routes.js
+++ src/interfaces/routes/routes.js
@@ -1,24 +1,22 @@
 const Controller = require('../../controllers/controllers')
 const express = require('express')
 const router = express.Router()
+const { errorMessages, generateError } = require('../../common/errors')
 const Controllers = require('../../controllers/controllers')
 
-router.get('/search/:city', async (req, res) => {
-    await Controllers.getCityDetails(req, res)
-})
-router.get('/', Controllers.get)
+router.get('/search/:city', Controllers.getCityDetails)
 
 router.get("*", (req, res) => {
     return res.status(404).send({
-        message: "resource not foud",
-    });
-});
+        message: errorMessages.resourceNotFound,
+    })
+})
 
 router.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send({
-        message: "Internal server error",
-    });
-});
+        message: errorMessages.internalServerError,
+    })
+})
 
 module.exports = router
