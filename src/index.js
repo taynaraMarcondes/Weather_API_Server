e2ec3058f79e2dae6cf2bbb3c2c085b1b6d236a2
taynaraMarcondes <taynaramarcondes@yahoo.com.br>
2021-03-05 04:26:47 -0300

    add body-parser

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const routes = require('./interfaces/routes/routes')
 
 const app = express()
@@ -7,6 +8,9 @@ const app = express()
 
 app.use(cors())
 app.options('*', cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.use('/', routes)
 
