fb8dd943eb67cb7e15ea7868d6f34af0836305d5
taynaraMarcondes <taynaramarcondes@yahoo.com.br>
2021-03-05 04:25:51 -0300

    db config

--- src/infrastructure/database/db.js
+++ src/infrastructure/database/db.js
@@ -1,12 +1,21 @@
 require('dotenv').config()
 const { Pool } = require('pg')
 const pool = new Pool({
-    user: 'postgres',
-    password: process.env.DATABASE_PASSWORD,
-    host: 'localhost',
-    port: 5432,
-    database: 'db_test'
+    user: process.env.USER_DB,
+    password: process.env.PASSWORD_DB,
+    host: process.env.HOST_DB,
+    port: process.env.PORT_DB,
+    database: process.env.DB_NAME
 })
+
+async function query(query, param) {
+    const client = await pool.connect()
+    try {
+        return await client.query(query, param)
+    } finally {
+        client.release()
+    }
+}
 
 pool.on('connect', () => {
     console.log('Database default connection is open')
@@ -16,6 +25,4 @@ pool.on('error', err => {
     console.error(`Unexpected error `, err)
 })
 
-module.exports = {
-    query: (text, params) => pool.query(text, params)
-}
+module.exports = { query }
